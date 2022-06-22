0ff9e47cd865962b4d669fd9935db86806052df2
Austin Layman <61898015+EnkiEnki77@users.noreply.github.com>
2022-06-22 13:08:01 -0700

    created footer component

--- components/BestGear.tsx
+++ components/BestGear.tsx
@@ -3,7 +3,7 @@ import bestGear from '../public/assets/shared/mobile/image-best-gear.jpg'
 
 const BestGear = () => {
   return (
-    <div className="px-6 ">
+    <div className="px-6 mb-[110px]">
         <img className=" h-[300px] rounded-lg mb-10" src={bestGear.src} alt="" />
         <h4 className="mb-8 text-center">Bringing you the <span className="text-brown">best</span> audio gear</h4>
         <p className="text-center">Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.</p>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import logo from '../public/assets/shared/desktop/logo.svg'
+import facebook from '../public/assets/shared/desktop/icon-facebook.svg'
+import twitter from '../public/assets/shared/desktop/icon-twitter.svg'
+import instagram from '../public/assets/shared/desktop/icon-instagram.svg'
 
 const Footer = () => {
+    const nav = ['home','headphones','speakers','earphones' ]
+    const social = [facebook,twitter,instagram]
   return (
-    <div>Footer</div>
+    <footer className="px-6 pt-[52px] pb-[38px] bg-black2 flex flex-col items-center gap-12">
+        <img src={logo.src} alt="" />
+        <div>{nav.map(item => <p className="sub-title text-white tracking-[2px] text-center mb-4">{item}</p>)}</div>
+        <p className="text-white opacity-50 text-center">Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers and sound specialists who are devoted to helping you get the most out of personal audio. Come and visit our demo facility - we’re open 7 days a week.</p>
+        <p className="text-white opacity-50">Copyright 2021. All Rights Reserved</p>
+        <div className='flex justify-center gap-4'>{social.map(item => <img className='w-6 h-auto' src={item.src}/>)}</div>
+    </footer>
   )
 }
 
