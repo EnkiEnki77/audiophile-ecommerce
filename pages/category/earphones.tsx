560d674b1ae8807a5abfa76d7526c5b812329d4e
Austin Layman <61898015+EnkiEnki77@users.noreply.github.com>
2022-06-23 18:18:01 -0700

    finished earphones category page

--- pages/category/earphones.tsx
+++ pages/category/earphones.tsx
@@ -1,14 +1,31 @@
 import React from 'react'
+import BestGear from '../../components/BestGear'
+import Categories from '../../components/Categories'
+import CategoryHeader from '../../components/CategoryHeader'
+import CategoryItems from '../../components/CategoryItems'
 import Footer from '../../components/Footer'
 import Header from '../../components/Header'
+import data from '../../data.json'
+import headphones from '../../public/assets/product-xx59-headphones/mobile/image-category-page-preview.jpg'
 
-const Earphones = () => {
+const Speakers = () => {
+  console.log(data)
+  const url = data[0].categoryImage.mobile.slice(1)
+  console.log(url)
+  const items = data.filter(item => {return item.category === 'earphones'})
   return (
     <>
       <Header/>
+      <CategoryHeader category='earphones'/>
+      <div className='flex flex-col  mt-16'>
+          {items.map(item => {return <CategoryItems key={item.id} img={headphones} 
+          productName={item.name} productDescript={item.description} productNew={item.new} slug={item.slug} category={item.category}/>})}
+      </div>
+      <Categories className='mb-[78px] pt-0'/>
+      <BestGear/>
       <Footer/>
     </>
   )
 }
 
-export default Earphones
+export default Speakers
